69097389e97cf5943ab6f569b9ca8a9fdfe280b7
Shashank Priyadarshi <57692808+shashank-priyadarshi@users.noreply.github.com>
2023-04-04 10:39:43 +0530

    Add files via upload

---
+++ read_secrets.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Read secrets from JSON file
+SECRETS_FILE="./secrets.json"
+SECRETS_JSON=$(cat $SECRETS_FILE)
+
+for key in $(echo "$SECRETS_JSON" | jq -r 'keys[]'); do
+    SECRET_NAME=$key
+    SECRET_VALUE=$(echo "$SECRETS_JSON" | jq -r ".$key")
+    echo "$SECRET_NAME has value $SECRET_VALUE"
+    export $SECRET_NAME="$SECRET_VALUE"
+done
